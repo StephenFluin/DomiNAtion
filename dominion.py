92eb3443f9bc21dd2608549399c19122626d4861
Stephen Fluin <stephen@mistuph.com>
2010-07-20 17:58:26 -0500

    Added several card types and card purchasing and acting

--- dominion.py
+++ dominion.py
@@ -11,7 +11,22 @@ def main():
 	runConsoleGame(g)
 	
 	
+
+def match(cardName, stack):
+	if isinstance(stack[0], Card):
+		
+		for c in stack:
+			if c.name.lower() == cardName.lower():
+				stack.remove(c)
+				return c
+	else:
+		for s in stack:
+			ret = match(cardName, s)
+			if ret:
+				return ret
 	
+	return None
+
 def runConsoleGame(game):
 	g = game
 	gameOver = False
@@ -19,54 +34,78 @@ def runConsoleGame(game):
 		#Give each player a turn
 		for p in g.players:
 			p.draw(g)
-			print "Hand:",
-			for c in p.hand:
-				print "%s" % (c.name),
+			p.showHand()
+			p.debugDeck()
+			p.showDiscard()
+			
+			print "Supplies:",
+			for c in g.supplies:
+				print "%s (%s)" % (c[0].name, len(c)),
 			print
-			actions = 1
-			buys = 1
+			p.actions = 1
+			p.buys = 1
 			
 			
 			#Action Phase
-			while actions > 0:
+			while p.actions > 0:
 				print "Enter the name of an action card or type skip:"
-				line = sys.stdin.readline()
-				line = line.strip('\n\r')
-				print "You entered '%s'." % (line)
-				actions -= 1
+				line = getCardName()
+				if line != "":
+					card = match(line,p.hand)
+					print "You entered '%s'." % (line)
+					if card and card.isActionable():
+						card.performAction(p, g)
+					else:
+						p.actions += 1
+				p.actions -= 1
 			
 			
 			#Buy Phase
-			while buys > 0:
+			while p.buys > 0:
 				print "Enter the name of a card you wish to buy:"
-				line = sys.stdin.readline()
-				line = line.strip('\n\r')
-				print "You entered '%s'." % (line)
-				buys -= 1
+				line = getCardName()
+				card = match(line,g.supplies)
+				if card:
+					p.discard.append(card)
+				p.buys -= 1
 			
 			p.endTurn()
+			
+def getCardName():
+	line = sys.stdin.readline()
+	line = line.strip('\n\r')
+	return line
 				
 
 class Game():
 	def __init__(self):
-		self.estates = []
-		self.duchies = []
-		self.provinces = []
-		self.golds = []
-		self.silvers = []
-		self.coppers = []
 		self.trash = []
 		self.players = []
 		
+		estates = []
+		duchies = []
+		provinces = []
+		golds = []
+		silvers = []
+		coppers = []
+		
+		self.supplies = []
+		
 		for i in range(12):
-			self.estates.append(Estate())
-			self.duchies.append(Duchy())
-			self.provinces.append(Province())
+			estates.append(Estate())
+			duchies.append(Duchy())
+			provinces.append(Province())
 			
 		for i in range(50):
-			self.golds.append(Gold())
-			self.silvers.append(Silver())
-			self.coppers.append(Copper())
+			golds.append(Gold())
+			silvers.append(Silver())
+			coppers.append(Copper())
+			
+		stack = range(10)
+		for i in range(10):
+			stack.append(Smithy())
+		self.supplies.extend([provinces, duchies, estates, golds, silvers, coppers])
+		self.supplies.extend(stack)
 	
 	def addPlayer(self,name):
 		self.players.append(Player(self,name))
@@ -79,13 +118,16 @@ class Player():
 		self.deck = []
 		self.discard = []
 		for i in range(7):
-			self.deck.append(game.coppers.pop())
+			self.deck.append(game.supplies[5].pop())
 		for i in range(3):
-			self.deck.append(game.estates.pop())
+			self.deck.append(game.supplies[2].pop())
 		random.shuffle(self.deck)
 			
 	def draw(self,game):
-		for i in range(5):
+		self.drawCards(game,5)
+		
+	def drawCards(self,game,count):
+		for i in range(count):
 			if len(self.deck) <= 0:
 				while len(self.discard) > 0:
 					self.deck.append(self.discard.pop())
@@ -95,7 +137,24 @@ class Player():
 	def endTurn(self):
 		while len(self.hand) >0:
 			self.discard.append(self.hand.pop())
-			
+	
+	def showHand(self):
+		print "Hand:",
+		for c in self.hand:
+			print "%s" % (c.name),
+		print
+		
+	def debugDeck(self):
+		print "Deck:",
+		for c in self.deck:
+			print "%s" % (c.name),
+		print
+		
+	def showDiscard(self):
+		print "Discard:",
+		for c in self.discard:
+			print "%s" % (c.name),
+		print
 			
 				
 				
@@ -109,14 +168,14 @@ class Card():
 	def getVictoryPoints(self):
 		pass
 	
-	def performAction(self,hand,deck,trash):
+	def performAction(self,actor,game):
 		pass
 	
 	def getCost(self):
 		return self.cost
 		
 	def isActionable(self):
-		return false;
+		return False;
 
 
 class VictoryCard(Card):
@@ -163,4 +222,37 @@ class Copper(Treasure):
 		self.name = "Copper"
 		self.worth = 1
 
+class Action(Card):
+	def isActionable(self):
+		return True
+
+class Smithy(Action):
+	def __init__(self):
+		Action.__init__(self,4)
+		self.name = "Smithy"
+		
+	def performAction(self, p, g):
+		p.drawCards(g,3)
+		p.showHand()
+class Laboratory(Action)
+	def __init__(self):
+		Action.__init__(self,5)
+		self.name = "Laboratory"
+	def performAction(self,p,g):
+		p.actions+= 1
+		p.drawCards(g,2)
+		p.showHand()
+		
+class Market(Action)
+	def __init__(self):
+		Action.__init__(self,5)
+		self.name = "Market"
+	def performAction(self,p,g):
+		p.actions+= 1
+		p.buys += 1
+		p.cash +=1
+		p.drawCards(g,1)
+		p.showHand()
+		
+		
 main()
